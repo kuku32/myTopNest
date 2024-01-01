9051f637e5aa2548dce2c6e24fbbcd9570d1d256
kuku20 <lluu3790@gmail.com>
2024-01-01 17:10:49 -0600

    done

--- src/top-manga/top-manga.controller.ts
+++ src/top-manga/top-manga.controller.ts
@@ -7,28 +7,43 @@ import { UpdateTopMangaDto } from './dto/update-top-manga.dto';
 export class TopMangaController {
   constructor(private readonly topMangaService: TopMangaService) {}
 
-  @Post()
-  create(@Body() createTopMangaDto: CreateTopMangaDto) {
-    return this.topMangaService.create(createTopMangaDto);
+  // @Post()
+  // create(@Body() createTopMangaDto: CreateTopMangaDto) {
+  //   return this.topMangaService.create(createTopMangaDto);
+  // }
+
+  // @Get()
+  // findAll() {
+  //   return this.topMangaService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.topMangaService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateTopMangaDto: UpdateTopMangaDto) {
+  //   return this.topMangaService.update(+id, updateTopMangaDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.topMangaService.remove(+id);
+  // }
+
+  @Get('story-lists')
+  getStoryLists(){
+    return this.topMangaService.getStoryLists()
   }
 
-  @Get()
-  findAll() {
-    return this.topMangaService.findAll();
+  @Get('story-lists/:type')
+  getStoryList(@Param('type') type: string){
+    return this.topMangaService.getStoryList(type)
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.topMangaService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTopMangaDto: UpdateTopMangaDto) {
-    return this.topMangaService.update(+id, updateTopMangaDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.topMangaService.remove(+id);
+  @Get('mangas/:type/:chapter')
+  getmangas(@Param('type') type: string,@Param('chapter') chapter: string){
+    return this.topMangaService.getmangas(type,chapter)
   }
 }
--- src/top-manga/top-manga.service.ts
+++ src/top-manga/top-manga.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException, UnauthorizedException } from '@nestjs/common';
 import { CreateTopMangaDto } from './dto/create-top-manga.dto';
 import { UpdateTopMangaDto } from './dto/update-top-manga.dto';
-
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 @Injectable()
 export class TopMangaService {
-  create(createTopMangaDto: CreateTopMangaDto) {
-    return 'This action adds a new topManga';
+  constructor(private readonly configService: ConfigService){}
+  async getStoryLists(){
+
+    try{
+      let BASE_URL = `${this.configService.get<any>('FIREBASE_DATA')}/story-lists.json`;
+      const response = await axios.get(BASE_URL);
+      return response?.data
+    }catch(error){
+      throw new UnauthorizedException("You're not allow");
+    }
   }
 
-  findAll() {
-    return `This action returns all topManga`;
+  async getStoryList(type:string){
+    try{
+      let BASE_URL = `${this.configService.get<any>('FIREBASE_DATA')}/story-lists/${type}.json`;
+      const response = await axios.get(BASE_URL);
+      return response?.data
+    }catch(error){
+      throw new UnauthorizedException("You're not allow");
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} topManga`;
-  }
-
-  update(id: number, updateTopMangaDto: UpdateTopMangaDto) {
-    return `This action updates a #${id} topManga`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} topManga`;
+  async getmangas(type:string,chapter:string){
+    try{
+      let BASE_URL = `${this.configService.get<any>('FIREBASE_DATA')}/mangas/${type}/${chapter}.json`;
+      const response = await axios.get(BASE_URL);
+      return response?.data
+    }catch(error){
+      throw new UnauthorizedException("You're not allow");
+    }
   }
 }
