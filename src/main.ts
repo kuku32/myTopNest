58b5eb15641c3132d2f07f2716d913e4c8f621a2
kuku20 <lluu3790@gmail.com>
2024-01-02 23:57:11 -0600

    update for loc

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200','https://eleceed.web.app','https://topmanga.vercel.app'],
+    origin: ['http://localhost:4200','https://eleceed.web.app','https://topmanga.vercel.app', 'https://topmanga-git-locreader-kuku20.vercel.app'],
     // origin: '*',
     credentials: true,
   });
