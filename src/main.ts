763bd14e43e2237e0cb65d8e87b5feb732b39f94
kuku20 <lluu3790@gmail.com>
2024-01-22 20:25:59 -0600

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200','https://eleceed.web.app','https://topmanga.vercel.app','https://topmanga-git-mainpostimage-kuku20.vercel.app'],
+    origin: ['http://localhost:4200','https://eleceed.web.app','https://topmanga.vercel.app','https://topmanga-git-mainpostimage-kuku20.vercel.app','https://topmanga-git-mainpostimagekitio-kuku20.vercel.app'],
     // origin: '*',
     credentials: true,
   });
