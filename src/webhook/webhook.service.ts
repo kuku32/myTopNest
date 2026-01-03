31221eb9de7bc459d2da165927211b7332295205
loc <lluu3790@gmail.com>
2026-01-03 15:21:12 -0600

    600

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -40,7 +40,7 @@ export class WebhookService {
       if(ticker.includes('USD')){
         ticker = this.stockHelperService.formatSymbol(ticker)
       }
-      let BASE_URL = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=${timefame}&outputsize=400&dp=2&apikey=${apikey}`;
+      let BASE_URL = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=${timefame}&outputsize=600&dp=2&apikey=${apikey}`;
       const response = await axios.get(BASE_URL);
       if (response.data.status === 'error') {
         throw new Error('API returned error status');
@@ -404,7 +404,7 @@ export class WebhookService {
       // return this.getCoinHistory(ticker, '5m')
       ticker = this.stockHelperService.formatSymbol(ticker)
     }
-    let BASE_URL = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=${tem}&outputsize=400&dp=2&apikey=`;
+    let BASE_URL = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=${tem}&outputsize=600&dp=2&apikey=`;
     console.log(BASE_URL)
     const response = await this.tryCatchtwelvedata(BASE_URL);
     if (response?.status == 'ok') {
