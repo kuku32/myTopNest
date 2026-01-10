e512955c7f17f0b7a728a6a0033fc5515844d286
loc <lluu3790@gmail.com>
2026-01-10 16:44:02 -0600

    Forest skip 15m

--- src/task_forex.service.ts
+++ src/task_forex.service.ts
@@ -61,7 +61,7 @@ export class TasksForexService {
     }
   }
 
-  @Cron('*/15 * * * *') // every 15 minutes
+  // @Cron('*/15 * * * *') // every 15 minutes
   async handle15minForex() {
     const tickers = [
       'CHFUSD',
