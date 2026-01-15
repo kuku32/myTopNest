43e0553c6a460cdfcc4fb5226d72381b2c28c202
loc <lluu3790@gmail.com>
2026-01-15 16:09:18 -0600

    more and more

--- src/send_everyday.service.ts
+++ src/send_everyday.service.ts
@@ -74,17 +74,22 @@ export class SendEverydayService {
     }
   }
 
+  async onModuleInit() {
+    // This runs ONCE when the app starts
+    // await this.delete();
+    // console.log(  stock_500_symbols.length)
+  }
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT) // close yesterday and open today
   async delete() {
     const yesterday = this.stockHelperService.getDateNDaysAgo(2);
     const Channels = [
+      'ERORR_CALL',
+      'CRON_CHECK',
       'US_ALL',
       'TSLA',
       'OTHER',
       'SMCI',
       'US_5M_HT',
-      'ERORR_CALL',
-      'CRON_CHECK',
       '15MIN_BUY_FX',
       '15MIN_SELL_FX',
       '30MIN_BUY_FX',
@@ -109,16 +114,37 @@ export class SendEverydayService {
       'CR_1H_BUY',
       'CR_30MIN_HT',
       'CR_30M_BUY',
+      'CR_5M_HT',
+      'CRYPTO_EARLY_5MIN',
+      'CR_15_HT',
+      'SELL_EARLY_DAY',
+      'BUY_EARLY_DAY',
+      'MA_BL_100_200',
+      'MA_BL_50_100',
+      'MA_BL_20_50',
+      'MA_BL_5_200',
+      'MA_BL_5_20',
+      'MA_AB_100_200',
+      'MA_AB_50_100',
+      'MA_AB_20_50',
+      'MA_AB_5_200',
+      'MA_AB_5_20',
+      'MACD_BL_POS',
+      'MACD_AB_POS',
+      'MACD_BL_NEG',
+      'MACD_AB_NEG',
+      'SRA_BL200',
+      'SRA_AB200',
     ]; // example list
 
-    await new Promise((resolve) => setTimeout(resolve, 2 * 60 * 1000));
+    await new Promise((resolve) => setTimeout(resolve, 10 * 60 * 1000));
     for (const channel of Channels) {
       // Log completion
-      this.logger.error(`✅ Finished sending for`, channel);
+      this.logger.log(`✅ Finished sending for`, channel);
 
       // DELETE two days ago messages
       await this.LocalPLWR.deleteMessages(channel, yesterday);
-      this.logger.error(`🗑️ Deleted old messages for`, channel);
+      this.logger.log(`🗑️ Deleted old messages for` + yesterday + channel);
     }
   }
 }
